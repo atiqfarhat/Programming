8570a2c018f433c411083905f4719c02fb14d5ca
Atiq Farhat <afnazi00@gmail.com>
2015-01-04 22:35:50 +0800

    Create HelloJuliet.java
    Basic Java Programming

---
+++ HelloJuliet.java
@@ -0,0 +1,7 @@
+public class HelloJuliet // creating class named HelloJuliet
+{
+public static void main(String[] args) // main method
+{
+System.out.println("Hello Juliet!!"); // print out the text
+}
+} 
